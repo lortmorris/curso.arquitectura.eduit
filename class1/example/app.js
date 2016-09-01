406d04178b21039e8706712878aa7867b0502681
cesar casas <cesarcasas@bsdsolutions.com.ar>
2016-09-01 19:51:17 -0300

    cluster random

--- class1/example/app.js
+++ class1/example/app.js
@@ -8,12 +8,10 @@ const myserver = http.createServer(app);
 const fs = require("fs");
 
 
-const airports = ["EZE","PE","MIA","AER","AUX","RET"];
+const airports = ["EZE","PE","MIA","AER","AUX","RET","LUL","POP","FRE","NIG","CBA"];
 
-const getRandomAirport =(airs)=>{
-	console.log("airs: " ,airs);	
-	return airs[ Math.floor(Math.random()*airs.length)];
-}
+const getRandomAirport =(airs)=> airs[ Math.floor(Math.random()*airs.length)];
+
 
 const getStop=(t)=>{
 	console.log(t);
@@ -37,24 +35,36 @@ const getStop=(t)=>{
 const getCluster = (template)=>{
 	let stops=[];
 	let limit = Math.floor(Math.random()*5);
+	
 
+	if(limit==0 || limit==1) return template;
 	let airs=airports.filter((a)=> !(a==template.from || a==template.to) );
 	
+	let rem = (a,e)=> a.filter( z=> z!=e );
+	let aux = null;
+
+	console.log("Generando cluster");
 
 	for(let x=0; x<=limit; x++){
 
 		let t =Object.assign({}, template);
 
-		if(x==0){			
-			t.to = getRandomAirport(airs);
-		}
-
-		if(x==limit){
-			t.from = getRandomAirport(airs);			
+		if(x==0){	
+			aux = getRandomAirport(airs);
+			airs = rem(airs, aux);
+			t.to = aux;
+		}else if(x==limit){
+			t.from = aux;
+		}else{
+			t.from = aux;
+			aux = getRandomAirport(airs);
+			airs = rem(airs, aux);
+			t.to = aux;
 		}
 		
+		
 		console.log("itera: ",x,limit,  t);		
-		//stops.push(getStop(t));
+		stops.push(getStop(t));
 	}
 	return stops;
 }
@@ -62,7 +72,7 @@ const getCluster = (template)=>{
 const getClusters = (template)=>{
 	let clusters = [];
 
-	for(let i=0; i<Math.random()*10; i++) 
+	for(let i=0; i<Math.random()*10; i++) 		
 		clusters.push(getCluster(template));
 	
 	return clusters;
@@ -75,6 +85,7 @@ let clusterTemplate={
 };
 
 app.get("/vuelos", (req, res)=>{
+	console.log("***************************************");
 	res.json(getClusters(clusterTemplate));
 });
 
