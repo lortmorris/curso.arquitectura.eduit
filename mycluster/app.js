c086c405ddd24c8fc2d81be0ea8162410ea30586
César Casas <cesarcasas@bsdsolutions.com.ar>
2017-01-24 21:15:23 -0300

    cluster example

--- mycluster/app.js
+++ mycluster/app.js
@@ -1,10 +1,30 @@
 const http = require('http');
+const express = require('express');
+const app = express();
+const os = require('os');
+const server = http.createServer(app);
+
 let counter=0;
-const server = http.createServer((req, res)=> {
-  res.end('gracias x su visita '+counter++);
+let PORT =  5000;
+
+app.get('/', (req, res)=> res.send('counter: '+counter++));
+app.get('/info', (req, res)=>{
+  res.json({
+    cpus: os.cpus(),
+    arch: os.arch(),
+    freemem: os.freemem(),
+    hostname: os.hostname(),
+    networks: os.networkInterfaces(),
+    avg: os.loadavg(),
+    pid: process.pid
+  });
 });
 
-let PORT =  5000;
+app.get('/break', (req, res)=> asd());
+
+setTimeout(()=>{
+  asdasd();
+}, Math.floor(Math.random()*60) * 1000);
 
 [...process.argv].forEach(ar=> {
   if(ar.indexOf('--port')>-1){
