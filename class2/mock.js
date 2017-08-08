8e05b98ee896ea4109375628dae1d5224a755615
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2017-08-08 20:29:27 -0300

    fix

--- class2/mock.js
+++ class2/mock.js
@@ -4,21 +4,32 @@ const products = ['Europa', 'Asia', 'USA', 'Viajeros'];
 const getRandom = (max) =>  Math.floor(Math.random() * max);
 const getRandomData = (data) =>  data[Math.floor(Math.random() * data.length)];
 
-const getRandomItem = () =>({
-  provider: getRandomData(providers),
-  producto: `${getRandomData(products)} ${getRandom(100)}`,
-  ageFrom: getRandom(5),
-  ageTo: getRandom(80) + 6,
-  daysFrom: getRandom(30),
-  dagysTo: getRandom(330) + 30,
-  fee: (Math.random() * 1000) + 40,
-});
+const getRandomItem = (type='module') =>{
+  const item = {
+    provider: getRandomData(providers),
+    producto: `${getRandomData(products)} ${getRandom(100)}`,
+    ageFrom: getRandom(5),
+    ageTo: getRandom(80) + 6,
+    fee: type === 'module' ? (Math.random() * 1000) + 40 : (Math.random() * 15 ) + 2,
+  };
+  if (type === 'module') {
+    item.daysFrom = getRandom(30);
+    item.dagysTo = getRandom(330) + 30;
+
+  }
+  return item;
+}
 
 const getMock = (age1, age2, age3, days) => {
-  const results = [];
-  for( let x=0; x<100; x++) results.push(getRandomItem());
+  const modulesPrice = [];
+  const daysPrice = [];
+  for( let x=0; x<100; x++) {
+    modulesPrice.push(getRandomItem('module'));
+    daysPrice.push(getRandomItem('days'))
+  }
   return {
-    results,
+    modulesPrice,
+    daysPrice,
   }
 }
 
