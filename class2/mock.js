ee2de6991b9e5db29830b3f167770ccf0dc0a58f
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2017-08-08 20:03:37 -0300

    fix

--- class2/mock.js
+++ class2/mock.js
@@ -1,7 +1,24 @@
 
+const providers = ['Assisto', 'Assiscard', 'TotalAsisto'];
+const products = ['Europa', 'Asia', 'USA', 'Viajeros'];
+const getRandom = (max) =>  Math.floor(Math.random() * max);
+const getRandomData = (data) =>  data[Math.floor(Math.random() * data.length)];
+
+const getRandomItem = () =>({
+  provider: getRandomData(providers),
+  producto: `${getRandomData(products)} ${getRandom(100)}`,
+  ageFrom: getRandom(5),
+  ageTo: getRandom(80) + 6,
+  daysFrom: getRandom(30),
+  dagysTo: getRandom(330) + 30,
+  fee: (Math.random() * 1000) + 40,
+});
+
 const getMock = (age1, age2, age3, days) => {
+  const results = [];
+  for( let x=0; x<100; x++) results.push(getRandomItem());
   return {
-    results:  []
+    results,
   }
 }
 
