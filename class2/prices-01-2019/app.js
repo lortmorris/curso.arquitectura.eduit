99d08f8d8bf866687f50a7ec7712f0038a2d2b48
cesar casas <cesarcasas@bsdsolutions.com.ar>
2019-01-03 21:16:45 -0300

    save

--- class2/prices-01-2019/app.js
+++ class2/prices-01-2019/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongojs = require('mongojs');
+const helmet = require('helmet');
+
 const cors = require('cors');
 const services = require('./services');
 const controllers = require('./controllers');
@@ -13,6 +15,7 @@ const port = 3000;
 const app = express();
 const server = http.createServer(app);
 
+app.use(helmet());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
