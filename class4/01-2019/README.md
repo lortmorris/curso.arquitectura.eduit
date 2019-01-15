538237f0cad41fae3c636e4cd8098d409672d3fb
cesar casas <cesarcasas@bsdsolutions.com.ar>
2019-01-15 20:56:42 -0300

    working

--- class4/01-2019/README.md
+++ class4/01-2019/README.md
@@ -2,3 +2,48 @@
 
 
 ## Redis
+
+
+
+Fb, Tt, email.
+
+Fb => fbid
+Tt => tid
+
+[email, email, email]
+
+if (db.users.findOne(email)) {
+  db.users.update({
+    fbid,
+    tid,
+    });
+} else {
+  db.users.insert({
+
+    });
+}
+
+
+## Frontend flows.
+
+Homa => Login API
+JWT <= Response
+
+
+## KEYS
+
+apikey: 1029381903uoiqndo1dij1o2d
+secretkey:   a/sdkalsjd98j91jx91jdoijdo1iio12j
+token <======= response
+
+
+## Products
+
+keycloak
+Auth0
+
+
+# Task
+
+passport: login fb.
+html: click login => show fb login window => show endpoint protected.
--- class4/01-2019/app.js
+++ class4/01-2019/app.js
@@ -3,23 +3,29 @@ const http = require('http');
 const passport = require('passport');
 const mongojs = require('mongojs');
 const express = require('express');
+const redis = require("redis")
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-
+const FileStore = require('session-file-store')(session);
 const LocalStrategy = require('passport-local').Strategy;
+const RedisStore = require('connect-redis')(session);
 
+const redisClient = redis.createClient();
 const db = mongojs('mongodb://localhost/cursoarq');
 const app = express();
 const server = http.createServer(app);
+
+redisClient.on("error", (err) => {
+    console.log("Error " + err);
+});
+
+
 passport.use(new LocalStrategy((username, password, done) => {
-    db.User.findOne({ username: username }, (err, user) => {
+    db.Users.findOne({ username, password }, (err, user) => {
       if (err) { return done(err); }
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
       }
       return done(null, user);
     });
@@ -27,15 +33,37 @@ passport.use(new LocalStrategy((username, password, done) => {
 ));
 
 
+
+const responsError = res => res.json({ success: false, apikey: 'error' });
+
+app.use((req, res, next) => {
+  if (req.headers.apikey) {
+    redisClient.get(req.headers.apikey, (err, doc) => {
+      console.info('info: ', err, doc);
+      if (err) return responsError(res);
+      if (!doc) return responsError(res);
+      return next();
+    });
+  }
+  return responsError(res);
+});
+
+
 app.use(express.static('public'));
 app.use(cookieParser());
-app.set('trust proxy', 1);
+
 app.use(session({
+  genid: (req) => {
+    console.info('generando id');
+    return new Date().getTime();
+  },
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
+  store: new RedisStore(),
 }));
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -43,12 +71,52 @@ app.use(bodyParser.json());
 
 
 
+/*
+app.post('/login1', (req, res) => {
+  db.Users.findOne({ username: req.body.username, password: req.body.password }, (err, doc) => {
+    if (doc) {
+      // req.session.user = doc;
+      return res.json({ login: 'ok'});
+    }
+    return res.redirect('/');
+  });
+});
+
+*/
+
+/*
+app.post('/adduser', (req, res) => {
+  db.Users.insert({
+    username: req.body.username,
+    password: req.body.password,
+    added: new Date(),
+  }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.json(doc);
+  });
+});
+*/
+
+app.get('/users', (req, res ) => {
+
+  console.info(req.session);
+  if (req.session.user) {
+    return db.Users.find({}, {}, (err, docs) => {
+        if (err) return res.json({ success: false });
+        return res.json({ success: true, docs });
+    });
+  }
+
+  return res.json({ success: false, error: 'NoLogin' });
+});
+
+
 app.get('/dashboard', (req, res) => res.json({ dash: 'ok' }));
+
 app.post('/login',
   passport.authenticate('local', { successRedirect: '/dashboard',
                                    failureRedirect: '/login',
                                    failureFlash: false })
 );
-
 
 server.listen(5000, () => console.info('listen on *:5000'));
